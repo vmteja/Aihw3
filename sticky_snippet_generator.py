2ec583df4ac14228ee8f8f90a159cd9cbeb5dcd4
Krishna Teja V <abstract470@gmail.com>
2017-11-02 14:55:28 -0700

    Add files via upload

---
+++ sticky_snippet_generator.py
@@ -0,0 +1,130 @@
+
+# coding: utf-8
+
+# In[84]:
+
+import numpy as np
+def mutate(listname, mutation_rate):
+    l = len(listname)
+    for i in range(0,l):
+        if(listname[i]=='A'):
+            ch = np.random.choice(
+          ['A','B', 'C', 'D'], 
+          1,
+          p=[1-mutation_rate,mutation_rate/3,mutation_rate/3,mutation_rate/3])
+            listname.append(ch[0])
+        elif(listname[i]=='B'):
+            ch = np.random.choice(
+          ['B','A', 'C', 'D'], 
+          1,
+          p=[1-mutation_rate,mutation_rate/3,mutation_rate/3,mutation_rate/3])
+            listname.append(ch[0])
+        elif(listname[i]=='C'):
+            ch = np.random.choice(
+          ['C','B', 'A', 'D'], 
+          1,
+          p=[1-mutation_rate,mutation_rate/3,mutation_rate/3, mutation_rate/3])
+            listname.append(ch[0])
+        else:
+            ch = np.random.choice(
+          ['D','A','B', 'C'], 
+          1,
+          p=[1-mutation_rate, mutation_rate/3,mutation_rate/3,mutation_rate/3])
+            listname.append(ch[0])
+    
+    
+        
+    return listname[l:]
+
+
+
+# In[85]:
+
+def generate_sticky():
+    v = np.random.choice(
+      ['A', 'B', 'C', 'D'], 
+      20,
+      p=[0.25, 0.25, 0.25, 0.25])
+
+    w=[]
+    for x in range(0,len(v)):
+        if(v[x] =='A'):
+            w.append('C')
+        elif(v[x] =='C'):
+            w.append('A')
+        elif(v[x] =='B'):
+            w.append('D')
+        elif(v[x] =='D'):
+            w.append('B')
+        
+
+    v = np.ndarray.tolist(v)
+    w=w[::-1] #w reverse
+    s = v+w
+    return s
+    
+
+
+# In[98]:
+
+import sys
+if __name__ == "__main__":
+    num_snippets = int(sys.argv[1])
+    mutation_rate = float(sys.argv[2])
+    from_ends = int(sys.argv[3])
+    output_file = sys.argv[4]
+    pfile = open(output_file,"w+")
+    pfile.seek(0)
+    pfile.truncate()
+    initial = generate_sticky()
+    n=0
+    
+    while(n<num_snippets):
+        f=np.random.choice(
+          ['start', 'end'], 
+          1,
+          p=[0.50,0.50])
+
+        if(f =='start'):
+            ends = initial[:from_ends]
+            middle=initial[from_ends:]
+    
+        else:
+            ends = initial[40-from_ends:]
+            middle = initial[:40-from_ends]
+    
+        ends = mutate(ends,1)
+        middle = mutate(middle,mutation_rate)
+    
+        if(f=='start'):
+            for x in range(0,from_ends):
+                middle.insert(x,ends[x])
+        elif(f=='end'):
+            for x in range(0,from_ends):
+                middle.append(ends[x])
+        output = open(output_file, "a+")
+        for char in middle:
+            output.write(char)
+        output.write("\n")
+        
+        n+=1
+        initial = middle
+        output.close()
+    
+    
+
+
+# In[100]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
