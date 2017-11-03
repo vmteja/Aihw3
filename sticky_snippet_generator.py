dbda32efed73c804d4e0f895ed36e4361d33ab25
Krishna Teja V <abstract470@gmail.com>
2017-11-03 16:33:28 -0700

    Update sticky_snippet_generator.py

--- sticky_snippet_generator.py
+++ sticky_snippet_generator.py
@@ -1,61 +1,112 @@
+
+
 import numpy as np
-import sys
-
-
-def get_random_other_character(char, ):
-    if char == 'A':
-        return str(np.random.choice(['B', 'C', 'D'], 1)[0])
-    elif char == 'B':
-        return str(np.random.choice(['A', 'C', 'D'], 1)[0])
-    elif char == 'C':
-        return str(np.random.choice(['A', 'B', 'D'], 1)[0])
-    else:
-        return str(np.random.choice(['A', 'B', 'C'], 1)[0])
-
-
-def mutate(string, rate, ends):
-    for i in range(len(string)):
-        if (0 <= i < ends) or ((len(string)-ends) <= i < len(string)):
-            decision = np.random.choice(['mutate', 'not_mutate'], 1, p=[rate, 1.0-rate])
-            if decision == 'mutate':
-                string[i] = get_random_other_character(string[i])
+def mutate(listname, mutation_rate):
+    l = len(listname)
+    for i in range(0,l):
+        if(listname[i]=='A'):
+            ch = np.random.choice(
+          ['A','B', 'C', 'D'], 
+          1,
+          p=[1-mutation_rate,mutation_rate/3,mutation_rate/3,mutation_rate/3])
+            listname.append(ch[0])
+        elif(listname[i]=='B'):
+            ch = np.random.choice(
+          ['B','A', 'C', 'D'], 
+          1,
+          p=[1-mutation_rate,mutation_rate/3,mutation_rate/3,mutation_rate/3])
+            listname.append(ch[0])
+        elif(listname[i]=='C'):
+            ch = np.random.choice(
+          ['C','B', 'A', 'D'], 
+          1,
+          p=[1-mutation_rate,mutation_rate/3,mutation_rate/3, mutation_rate/3])
+            listname.append(ch[0])
         else:
-            string[i] = get_random_other_character(string[i])
-
-    return string
-
-
-def randomly_generate_stick_palindrome():
-    letters = ['A', 'B', 'C', 'D']
-    first_half = np.random.choice(letters, 10)
-    last_half = []
-
-    for letter in first_half:
-        if letter == 'A':
-            last_half.append('C')
-        elif letter == 'B':
-            last_half.append('D')
-        elif letter == 'C':
-            last_half.append('A')
-        else:
-            last_half.append('B')
-
-    first_half = np.ndarray.tolist(first_half)
-    last_half = last_half[::-1]  # w reverse
-    return first_half + last_half
+            ch = np.random.choice(
+          ['D','A','B', 'C'], 
+          1,
+          p=[1-mutation_rate, mutation_rate/3,mutation_rate/3,mutation_rate/3])
+            listname.append(ch[0])
+    
+    
+        
+    return listname[l:]
 
 
-# python sticky_snippet_generator.py num_snippets mutation_rate from_ends output_file
-if __name__ == "_main_":
+
+# In[85]:
+
+def generate_sticky():
+    v = np.random.choice(
+      ['A', 'B', 'C', 'D'], 
+      20,
+      p=[0.25, 0.25, 0.25, 0.25])
+
+    w=[]
+    for x in range(0,len(v)):
+        if(v[x] =='A'):
+            w.append('C')
+        elif(v[x] =='C'):
+            w.append('A')
+        elif(v[x] =='B'):
+            w.append('D')
+        elif(v[x] =='D'):
+            w.append('B')
+        
+
+    v = np.ndarray.tolist(v)
+    w=w[::-1] #w reverse
+    s = v+w
+    return s
+    
+
+
+# In[98]:
+
+
+if __name__ == "__main__":
     num_snippets = int(sys.argv[1])
     mutation_rate = float(sys.argv[2])
     from_ends = int(sys.argv[3])
     output_file = sys.argv[4]
+    pfile = open(output_file,"w+")
+    pfile.seek(0)
+    pfile.truncate()
+    initial = generate_sticky()
+    n=0
+    
+    while(n<num_snippets):
+        f=np.random.choice(
+          ['start', 'end'], 
+          1,
+          p=[0.50,0.50])
 
-    initial_stick_palindrome = randomly_generate_stick_palindrome()
-    while num_snippets:
-        string_after_mutation = mutate(initial_stick_palindrome, mutation_rate, from_ends)
-        with open(output_file, 'a') as f:
-            f.write(''.join(string_after_mutation) + '\n')
-        f.close()
-        num_snippets -= 1
+        if(f =='start'):
+            ends = initial[:from_ends]
+            middle=initial[from_ends:]
+    
+        else:
+            ends = initial[40-from_ends:]
+            middle = initial[:40-from_ends]
+    
+        ends = mutate(ends,1)
+        middle = mutate(middle,mutation_rate)
+    
+        if(f=='start'):
+            for x in range(0,from_ends):
+                middle.insert(x,ends[x])
+        elif(f=='end'):
+            for x in range(0,from_ends):
+                middle.append(ends[x])
+        output = open(output_file, "a+")
+        for char in middle:
+            output.write(char)
+        output.write("\n")
+        
+        n+=1
+        initial = middle
+        output.close()
+    
+    
+
